12e6e6a2563dd5faac85acbc221193bcdb8ce4af
Md Tanjeem Haider <mdtanjeem.haider@ontariotechu.net>
2023-04-10 14:14:19 -0400

    project base

---
+++ src/main/java/com/example/w23csci2020uprojectteam43/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.w23csci2020uprojectteam43;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/com/example/w23csci2020uprojectteam43/HelloResource.java
@@ -0,0 +1,14 @@
+package com.example.w23csci2020uprojectteam43;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
